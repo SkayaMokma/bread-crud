ac6d91ec3188c8d5f9feaa270f913873934e8622
SkayaMokma <skayamokma@outlook.com>
2023-05-07 00:21:57 -0400

    Delete and update bread

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show({ bread }) {
+function Show({ bread, index }) {
     return (
         <Default>
             <h3>{bread.name}</h3>
@@ -13,6 +13,12 @@ function Show({ bread }) {
                 have gluten
             </p>
             <img src={bread.image} alt={bread.name} />
+            <a href={`/breads/${index}/edit`}>
+                <button>Edit</button>
+            </a>
+            <form action={`/breads/${index}?_method=DELETE`} method='POST'>
+                <input type='submit' value='DELETE'/>
+            </form>
             <li>
                 <a href={'/breads'}>Go home</a>
             </li>
